33035a5eb376e6eebdaa3141a17b3321c6c8120b
somersby <somersby@kakao.com>
2020-01-02 01:04:09 +0900

    first commit

---
+++ app/src/main/java/io/github/somersbii/libraryinfo/Model/SeatInfoCrawling.java
@@ -0,0 +1,9 @@
+package io.github.somersbii.libraryinfo.Model;
+
+public class SeatInfoCrawling {
+    //Jsoup은 Javascript를 지원하지 않기 때문에 동적 페이지의 정보를 가져올 수 없다.
+    //Selenium을 사용해 동적 페이지의 정보를 가져온다.
+
+
+
+}
